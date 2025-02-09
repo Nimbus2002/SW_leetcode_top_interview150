1f5f0442ad9a3e410e0b4ea17fb30fe04ae9caf7
Ellen Jieun Oh <66589193+Nimbus2002@users.noreply.github.com>
2025-02-09 16:35:17 +0900

    Update remove_duplicates_from_sorted_arrary2.py

--- remove_duplicates_from_sorted_arrary2.py
+++ remove_duplicates_from_sorted_arrary2.py
@@ -15,3 +15,16 @@ class Solution:
                     nums[k] = nums[i]
                     k +=1
         return k
+
+
+
+## 더 좋음 
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k =2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[k - 2]:
+                nums[k] = nums[i]
+                k += 1
+        return k
+        
