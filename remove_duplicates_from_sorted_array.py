5252fe7b799f4bbaad3823e220072b22848a6bec
Ellen Jieun Oh <66589193+Nimbus2002@users.noreply.github.com>
2025-02-09 15:58:43 +0900

    Update remove_duplicates_from_sorted_array.py

--- remove_duplicates_from_sorted_array.py
+++ remove_duplicates_from_sorted_array.py
@@ -9,3 +9,14 @@ class Solution:
                 # print(nums)
             numsin.append(i)
         return len(nums)
+
+
+## 더 좋은 방법
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[j] = nums[i]
+                j +=1
+        return j
