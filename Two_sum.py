da29ff2996be05d7bc57e22955c2aa2cd86d4655
Ellen Jieun Oh <66589193+Nimbus2002@users.noreply.github.com>
2025-06-15 23:56:09 +0900

    Update Two_sum.py

--- Two_sum.py
+++ Two_sum.py
@@ -1,9 +1,34 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # solution (1) brute force
+#        # solution (1) brute force
+        # n = len(nums)
+        # for i in range(n-1):
+        #     for j in range(i+1, n):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
+        # return []
+
+        # # solution (2) - hasp map즉, dic에서 key찾는건 o(1)
+        # nummaps ={}
+        # n = len(nums)
+
+        # for i in range(n):
+        #     nummaps[nums[i]] = i 
+
+        # for i in range(n):
+        #     complement = target - nums[i]
+        #     if complement in nummaps and nummaps[complement] != i:
+        #         return [i, nummaps[complement]]
+
+        # return []
+        # solution 3 - one pass hash table
+        nummap = {}
         n = len(nums)
-        for i in range(n-1):
-            for j in range(i+1, n):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in nummap:
+                return [nummap[complement], i]
+            nummap[nums[i]] = i 
+
         return []
